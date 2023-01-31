6e99ee65a722df7174bd4032e98e34c7f4b4063d
Evan Jacobs <probablyup@gmail.com>
2023-01-31 17:18:45 -0500

    fix: lists are not eligible in simple inline mode
    Fixes #466
    
    Signed-off-by: Innei <i@innei.in>

--- index.tsx
+++ index.tsx
@@ -546,7 +546,7 @@ function generateListRule(h: any, type: LIST_TYPE) {
       // in which case we can parse with inline scope, but need to allow
       // nested lists inside this inline scope.
       const isStartOfLine = LIST_LOOKBEHIND_R.exec(prevCapture)
-      const isListBlock = state.list || !state.inline
+      const isListBlock = state.list || (!state.inline && !state.simple)
 
       if (isStartOfLine && isListBlock) {
         source = isStartOfLine[1] + source
